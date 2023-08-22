85364c5fefd706171005c5d998cd1d3f4015e8eb
Ngo Xuan Nghia <JohnNgo1311@gmail.com>
2023-08-23 00:20:18 +0700

    onesignal

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:mobile_health_check/presentation/common_widget/dialog/show_toast.dart';
 import 'package:mobile_health_check/presentation/route/route_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'classes/language_constant.dart';
 import 'di/di.dart';
 import 'package:camera/camera.dart';
@@ -9,12 +11,14 @@ import 'presentation/route/route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile_health_check/common/service/firebase/firebase_options.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 List<CameraDescription> cameras = [];
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await initOneSignal();
   try {
     cameras = await availableCameras();
   } on CameraException catch (e) {
@@ -26,6 +30,58 @@ Future<void> main() async {
       statusBarIconBrightness: Brightness.dark));
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+}
+
+Future<void> initOneSignal() async {
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("eb1e614e-54fe-4824-9c1a-aad236ec92d3");
+// The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  final status = await Permission.notification.request();
+  if (status.isDenied) {
+    await Permission.notification.request();
+    showToast("Bạn chưa bật cho phép thông báo");
+    //SHOW DIALOG with 2 button => setting and cancel, if user press "setting" =>  final status = await OneSignal.Notifications.requestPermission(true);
+    // We didn't ask for permission yet or the permission has been denied before but not permanently.
+    //  await openAppSettings(); ==> nằm trong Dialog làm phía trên
+  }
+  OneSignal.User.pushSubscription.addObserver((state) {
+    print(state.current.jsonRepresentation());
+  });
+  OneSignal.Notifications.addPermissionObserver((state) {
+    print("Has permission $state");
+  });
+  OneSignal.Notifications.addClickListener((event) {
+    print('NOTIFICATION CLICK LISTENER CALLED WITH EVENT: $event');
+    // Navigator.pushNamed(, routeName)
+  });
+
+  OneSignal.Notifications.addForegroundWillDisplayListener((event) {
+    /// preventDefault to not display the notification
+    event.preventDefault();
+
+    /// Do async work
+    // debugPrint("^^${event.notification.body}");
+
+    /// notification.display() to display after preventing default
+    event.notification.display();
+  });
+  // OneSignal.InAppMessages.addClickListener((event) {
+  //   print(
+  //       "In App Message Clicked: \n${event.result.jsonRepresentation().replaceAll("\\n", "\n")}");
+  // });
+  // OneSignal.InAppMessages.addWillDisplayListener((event) {
+  //   print("ON WILL DISPLAY IN APP MESSAGE ${event.message.messageId}");
+  // });
+  // OneSignal.InAppMessages.addDidDisplayListener((event) {
+  //   print("ON DID DISPLAY IN APP MESSAGE ${event.message.messageId}");
+  // });
+  // OneSignal.InAppMessages.addWillDismissListener((event) {
+  //   print("ON WILL DISMISS IN APP MESSAGE ${event.message.messageId}");
+  // });
+  // OneSignal.InAppMessages.addDidDismissListener((event) {
+  //   print("ON DID DISMISS IN APP MESSAGE ${event.message.messageId}");
+  // });
 }
 
 class MyApp extends StatefulWidget {
--- lib/presentation/common_widget/dialog/show_toast.dart
+++ lib/presentation/common_widget/dialog/show_toast.dart
@@ -6,7 +6,7 @@ void showToast(String message) {
   Fluttertoast.showToast(
     msg: message,
     toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.CENTER,
+    gravity: ToastGravity.BOTTOM,
     timeInSecForIosWeb: 1,
     backgroundColor: const Color(0xffCCECF9),
     textColor: Colors.black,
